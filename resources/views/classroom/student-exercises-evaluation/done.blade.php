b3b2af87069dac84d3acda75f5065e9a87494d00
Adriano Ramalho <adriano.ramalho2121@gmail.com>
2020-11-23 16:18:53 +0000

    auth()->user()->user_role_id fixed;

--- resources/views/classroom/student-exercises-evaluation/done.blade.php
+++ resources/views/classroom/student-exercises-evaluation/done.blade.php
@@ -14,7 +14,7 @@
                     <strong>Avaliação:</strong> <strong class="exercise_complete"> 100%</strong>
                 </p>
             </div>
-            @if(auth()->user()->role == 1 || auth()->user()->role == 2)
+            @if(auth()->user()->user_role_id == 1 || auth()->user()->user_role_id == 2)
                 <div class="d-block float-right mt-3">
                     <a href="#" hidden class="btn search-btn comment_submit" style="float: none; padding: 12px 20px; margin-left: 15px;">
                         <img src="{{asset('/assets/backoffice_assets/icons/Eye.svg')}}" alt="" style="margin-right: 5px; margin-bottom: 2px;">
@@ -65,7 +65,7 @@
                     <strong>Avaliação:</strong> <strong class="exercise_complete"> 100%</strong>
                 </p>
             </div>
-            @if(auth()->user()->role == 1 || auth()->user()->role == 2)
+            @if(auth()->user()->user_role_id == 1 || auth()->user()->user_role_id == 2)
                 <div class="d-block float-right mt-3">
                     <a href="#" hidden class="btn search-btn comment_submit" style="float: none; padding: 12px 20px; margin-left: 15px;">
                         <img src="{{asset('/assets/backoffice_assets/icons/Eye.svg')}}" alt="" style="margin-right: 5px; margin-bottom: 2px;">
@@ -116,7 +116,7 @@
                     <strong>Avaliação:</strong> <strong class="exercise_complete"> 100%</strong>
                 </p>
             </div>
-            @if(auth()->user()->role == 1 || auth()->user()->role == 2)
+            @if(auth()->user()->user_role_id == 1 || auth()->user()->user_role_id == 2)
                 <div class="d-block float-right mt-3">
                     <a href="#" hidden class="btn search-btn comment_submit" style="float: none; padding: 12px 20px; margin-left: 15px;">
                         <img src="{{asset('/assets/backoffice_assets/icons/Eye.svg')}}" alt="" style="margin-right: 5px; margin-bottom: 2px;">
--- resources/views/classroom/student-exercises-evaluation/in-course.blade.php
+++ resources/views/classroom/student-exercises-evaluation/in-course.blade.php
@@ -12,7 +12,7 @@
                     <strong>Avaliação Provisória:</strong> <strong class="exercise_awaiting"> 80%</strong>
                 </p> --}}
             </div>
-            @if(auth()->user()->role == 1 || auth()->user()->role == 2)
+            @if(auth()->user()->user_role_id == 1 || auth()->user()->user_role_id == 2)
                 <div class="d-block float-right">
                     <a href="#" hidden class="btn search-btn comment_submit" style="float: none; padding: 12px 20px; margin-left: 15px;">
                         <img src="{{asset('/assets/backoffice_assets/icons/Eye.svg')}}" alt="" style="margin-right: 5px; margin-bottom: 2px;">
@@ -61,7 +61,7 @@
                     <strong>Avaliação Provisória:</strong> <strong class="exercise_awaiting"> 80%</strong>
                 </p> --}}
             </div>
-            @if(auth()->user()->role == 1 || auth()->user()->role == 2)
+            @if(auth()->user()->user_role_id == 1 || auth()->user()->user_role_id == 2)
                 <div class="d-block float-right">
                     <a href="#" hidden class="btn search-btn comment_submit" style="float: none; padding: 12px 20px; margin-left: 15px;">
                         <img src="{{asset('/assets/backoffice_assets/icons/Eye.svg')}}" alt="" style="margin-right: 5px; margin-bottom: 2px;">
@@ -110,7 +110,7 @@
                     <strong>Avaliação Provisória:</strong> <strong class="exercise_awaiting"> 80%</strong>
                 </p> --}}
             </div>
-            @if(auth()->user()->role == 1 || auth()->user()->role == 2)
+            @if(auth()->user()->user_role_id == 1 || auth()->user()->user_role_id == 2)
                 <div class="d-block float-right">
                     <a href="#" hidden class="btn search-btn comment_submit" style="float: none; padding: 12px 20px; margin-left: 15px;">
                         <img src="{{asset('/assets/backoffice_assets/icons/Eye.svg')}}" alt="" style="margin-right: 5px; margin-bottom: 2px;">
