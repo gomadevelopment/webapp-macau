1f29770eed54bca0a329055de6c62f6ec418ba51
Adriano Ramalho <adriano.ramalho2121@gmail.com>
2022-05-17 15:46:34 +0100

    added middleware that prevents requests from outside Macau ("X-Country" != "MO") being made;

---
+++ app/Http/Middleware/NotAvailableByCountry.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class NotAvailableByCountry
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->header('X-Country') != 'MO') {
+            abort(403, 'Not available for your country.');
+        }
+
+        return $next($request);
+    }
+}
